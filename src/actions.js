68a970f52fba167bf2be30e0f5429a419480abaa
Evandro Rodrigues <43556689+evanrodr@users.noreply.github.com>
2020-02-07 14:39:55 -0300

    polishing the project

--- src/actions.js
+++ src/actions.js
@@ -1,3 +1,5 @@
+import { apiCall } from './api/api'
+
 import {
   CHANGE_SEARCH_FIELD,
   REQUEST_ROBOTS_PENDING,
@@ -9,8 +11,7 @@ export const setSearchField = (text) => ({ type: CHANGE_SEARCH_FIELD, payload: t
 
 export const requestRobots = () => (dispatch) => {
   dispatch({ type: REQUEST_ROBOTS_PENDING });
-  fetch('https://jsonplaceholder.typicode.com/users')
-    .then(response => response.json())
+  apiCall('https://jsonplaceholder.typicode.com/users')
     .then(data => dispatch({ type: REQUEST_ROBOTS_SUCCESS, payload: data }))
     .catch(error => dispatch({ type: REQUEST_ROBOTS_FAILED, payload: error }))
 }
